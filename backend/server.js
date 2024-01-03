479a633e91d2dc725aad6e1eee209c2ae7f24fea
jawadshiekh <jawadshiekh.internativelabs@gmail.com>
2024-01-03 12:05:34 -0800

    mysql

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,25 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const pool = mysql.createPool({
+  host: "144.126.218.194",
+  user: "root",
+  password: "password",
+  database: "todos",
+  port: 3306,
 });
 
+app.get("/", async (req, res) => {
+  const [todos] = await pool.query(`SELECT * FROM todos`);
+
+  res.send(todos);
+});
 app.get("/about", function (req, res) {
   res.send("lorem ipsum dolor .......");
 });
